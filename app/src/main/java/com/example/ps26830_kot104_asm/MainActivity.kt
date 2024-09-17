dc3bd0c5a8a70792ad9a5efd45a558b655de3bda
devLeoNguyen <151083002+devLeoNguyn@users.noreply.github.com>
2024-09-17 18:17:08 +0700

    Add UI for boarding screen

--- app/src/main/java/com/example/ps26830_kot104_asm/MainActivity.kt
+++ app/src/main/java/com/example/ps26830_kot104_asm/MainActivity.kt
@@ -16,31 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PS26830_KOT104_ASMTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PS26830_KOT104_ASMTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/ps26830_kot104_asm/presentation/onboarding/components/OnBoardingPage.kt
@@ -0,0 +1,16 @@
+package com.example.ps26830_kot104_asm.presentation.onboarding.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun OnBoardingPage() {
+    Column(modifier = Modifier) {
+        Image(painter = painterResource(id = ), contentDescription = )
+    }
+}
